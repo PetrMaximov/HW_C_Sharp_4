9214339a4be23e9cc608f888b9b28b3b34e487fb
«Petr» <maxpetrb@mail.ru>
2023-09-18 20:23:01 +0300

    Task27 Сумма цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int SumNum (int Num)
+{
+    int Sum = 0;
+    Num = Math.Abs(Num);
+    while (Num > 0)
+    {
+       int digit = Num % 10;
+       Sum = Sum + digit;
+       Num = Num / 10;
+    }
+    return Sum;
+}
+
+int res = SumNum (9012);
+Console.WriteLine(res);
