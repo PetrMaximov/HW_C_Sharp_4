7f17b93ce6693a7e3f2ef57f7c54c0ec67958815
«Petr» <maxpetrb@mail.ru>
2023-09-18 20:47:51 +0300

    Task29 массив из N элементов

---
+++ Task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] NArray (int N)
+{
+    int[] A = new int[N];
+    while (N > 0)
+    {
+        A[N-1] = new Random().Next(0,100);
+        N = N - 1;
+    }
+    return A; 
+}
+
+int[] res = NArray(8);
+var strres = string.Join(" ", res);
+Console.WriteLine(strres);
