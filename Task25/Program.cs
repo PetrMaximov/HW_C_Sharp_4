fca1162c64905dc5123ee521c5cd4973dbe44c5e
«Petr» <maxpetrb@mail.ru>
2023-09-18 20:05:19 +0300

    Task25 Возведение в степень

---
+++ Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Stepen(int A, int B)
+{
+    int Step = 1;
+    while (B != 0) {
+        Step = Step * A;
+        B--;
+    }
+return Step;
+}
+
+int res = Stepen(4,5);
+Console.WriteLine(res);
